fd394f205b4015b2d07fb0cd6b681451c64b5289
misfist (Patricia) <pea@misfist.com>
2011-12-03 18:21:14 -0500

    Changed Appearance of Search
    Changed search so it appears to be in the buddy bar.

--- wp-content/themes/nycga/header.php
+++ wp-content/themes/nycga/header.php
@@ -20,27 +20,31 @@
 	<body <?php body_class() ?> id="bp-default">
 		<div id="header-section">
 			<?php do_action( 'bp_before_header' ) ?>
-			<div id="search-bar" role="search">
+			
+			<!--#search-bar-->
+			<div id="search-bar">
 				<div class="padder">
 					<div class="align-right">
-					
-					<form action="<?php echo bp_search_form_action() ?>" method="post" id="search-form">
-						<label for="search-terms" class="accessibly-hidden"><?php _e( 'Search for:', 'buddypress' ); ?></label>
-						<input type="search" id="search-terms" name="search-terms" value="<?php echo isset( $_REQUEST['s'] ) ? esc_attr( $_REQUEST['s'] ) : ''; ?>" />
-			
-						<?php echo bp_search_form_type_select() ?>
-			
-						<input type="submit" name="search-submit" id="search-submit" value="<?php _e( 'Search', 'buddypress' ) ?>" />
-			
-						<?php wp_nonce_field( 'bp_search_form' ) ?>
-			
-					</form><!-- #search-form -->
-			
-				<?php do_action( 'bp_search_login_bar' ) ?>
+						<!-- #search-form -->
+						<form action="<?php echo bp_search_form_action() ?>" method="post" id="search-form">
+							<label for="search-terms" class="accessibly-hidden"><?php _e( 'Search for:', 'buddypress' ); ?></label>
+							<input type="search" id="search-terms" name="search-terms" value="<?php echo isset( $_REQUEST['s'] ) ? esc_attr( $_REQUEST['s'] ) : ''; ?>" />
+							
+							<?php echo bp_search_form_type_select() ?>
+							
+							<input type="submit" name="search-submit" id="search-submit" value="<?php _e( 'Search', 'buddypress' ) ?>" />
+							
+							<?php wp_nonce_field( 'bp_search_form' ) ?>
+						
+						</form><!-- /#search-form -->
+						
+						<?php do_action( 'bp_search_login_bar' ) ?>
+					</div>
 				</div>
 			</div>
-		</div><!-- #search-bar -->
-		<div id="header">
+			<!-- /#search-bar-->
+			
+			<div id="header">
 			
 				<div class="padder">
 					<div id="header-link"><a href="<?php echo home_url(); ?>" title="<?php _ex( 'Home', 'Home page banner link title', 'buddypress' ); ?>"></a>
