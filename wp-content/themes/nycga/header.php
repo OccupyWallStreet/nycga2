a0f3fa726dc8f086baca17b9df4429357e3bd26a
root <root@nycgastage.(none)>
2011-12-07 05:32:00 -0500

    Revert "Positioned Search in Buddybar"
    This reverts commit 0b4a274596da48648e2b4ebc783f1313bd9eae5b.

--- wp-content/themes/nycga/header.php
+++ wp-content/themes/nycga/header.php
@@ -19,27 +19,22 @@
 
 	<body <?php body_class() ?> id="bp-default">
 		<div id="header-section">
-			<?php do_action( 'bp_before_header' ) ?>
-			<div id="search-bar" role="search">
-				<div class="padder">
-					<div class="align-right">
-					
-					<form action="<?php echo bp_search_form_action() ?>" method="post" id="search-form">
-						<label for="search-terms" class="accessibly-hidden"><?php _e( 'Search for:', 'buddypress' ); ?></label>
-						<input type="text" id="search-terms" name="search-terms" value="<?php echo isset( $_REQUEST['s'] ) ? esc_attr( $_REQUEST['s'] ) : ''; ?>" />
-			
-						<?php echo bp_search_form_type_select() ?>
-			
-						<input type="submit" name="search-submit" id="search-submit" value="<?php _e( 'Search', 'buddypress' ) ?>" />
-			
-						<?php wp_nonce_field( 'bp_search_form' ) ?>
-			
-					</form><!-- #search-form -->
-			
-				<?php do_action( 'bp_search_login_bar' ) ?>
-				</div>
-			</div>
-		</div><!-- #search-bar -->
+		<?php do_action( 'bp_before_header' ) ?>
+		<div id="search-bar" role="search">
+		<form action="<?php echo bp_search_form_action() ?>" method="post" id="search-form">
+			<label for="search-terms" class="accessibly-hidden"><?php _e( 'Search for:', 'buddypress' ); ?></label>
+			<input type="text" id="search-terms" name="search-terms" value="<?php echo isset( $_REQUEST['s'] ) ? esc_attr( $_REQUEST['s'] ) : ''; ?>" />
+
+			<?php echo bp_search_form_type_select() ?>
+
+			<input type="submit" name="search-submit" id="search-submit" value="<?php _e( 'Search', 'buddypress' ) ?>" />
+
+			<?php wp_nonce_field( 'bp_search_form' ) ?>
+
+		</form><!-- #search-form -->
+
+<?php do_action( 'bp_search_login_bar' ) ?>
+	</div><!-- #search-bar -->
 		<div id="header">
 			
 				<div class="padder">
