6dc3f6fb70a17bf53c8314b89fe48078eac4c128
Thiago de Mello Bueno <thiago@madeofpeople.org>
2011-12-19 02:56:11 -0500

    fixed issue with css and header not showing up in certain conditions of header

--- wp-content/themes/nycga2012/page-nycga-home.php
+++ wp-content/themes/nycga2012/page-nycga-home.php
@@ -3,35 +3,33 @@
 Template Name: NYCGA Homepage
 */
 ?>
-
 <?php get_header() ?>
-
-	<?php locate_template( array( 'leftsidebar.php' ), true ) ?>
-	<div id="content" class="grid_15">
+<?php locate_template( array( 'leftsidebar.php' ), true ) ?>
+<div id="content" class="grid_15">
 	<?php do_action( 'bp_before_blog_page' ) ?>
-		<div class="page" id="blog-page" role="main">
-
-<?php $pages = get_pages(''); if($pages): foreach($pages as $page):?>
-
-
-			<h2 class="pagetitle"><?php the_title(); ?></h2>
-			<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-				<div class="entry">
-					<?php the_content( __( '<p class="serif">Read the rest of this page &rarr;</p>', 'buddypress' ) ); ?>
-					<?php wp_link_pages( array( 'before' => '<div class="page-link"><p>' . __( 'Pages: ', 'buddypress' ), 'after' => '</p></div>', 'next_or_number' => 'number' ) ); ?>
-					<?php edit_post_link( __( 'Edit this page.', 'buddypress' ), '<p class="edit-link">', '</p>'); ?>
-				</div>
+	<div class="page" id="blog-page" role="main">
+		<?php 
+			$pages = get_pages('');
+			if($pages): 
+			foreach($pages as $page):
+		?>
+		<h2 class="pagetitle"><?php the_title(); ?></h2>
+		<div id="post-<?php the_ID(); ?>" <?php post_class();?>>
+			<div class="entry">
+				<?php the_content( __( '<p class="serif">Read the rest of this page &rarr;</p>', 'buddypress' ) ); ?>
+				<?php wp_link_pages( array( 'before' => '<div class="page-link"><p>' . __( 'Pages: ', 'buddypress' ), 'after' => '</p></div>', 'next_or_number' => 'number' ) ); ?>
+				<?php edit_post_link( __( 'Edit this page.', 'buddypress' ), '<p class="edit-link">', '</p>'); ?>
 			</div>
-
-		<?php comments_template(); ?>
-
-<?php endwhile; endif;?>
-		</div><!-- .page -->
-		<?php do_action( 'bp_after_blog_page' ) ?>
-	</div><!-- #content -->
-
-	<?php get_sidebar() ?>
-
+		</div>
+		<?php comments_template();?>
+		<?php
+			endforeach;
+			endif;
+		?>
+	</div><!-- .page -->
+	<?php do_action( 'bp_after_blog_page' ); ?>
+</div><!-- #content -->
+<?php get_sidebar() ?>
 <?php get_footer(); ?>
 
 
