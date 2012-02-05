79beb4dd0c71615b67a3cc8ecdf90cf04170c02a
Louie McCoy <louie@louiemccoy.com>
2012-02-05 16:50:12 -0500

    first draft of content display

---
+++ wp-content/plugins/ga-group-info/info-tab-content.php
@@ -0,0 +1,13 @@
+<?php
+// Get full set of options
+// put them into a multi-dimensional array
+gait_display_info_content();
+
+function gait_display_info_content(){ ?>
+<div class="extra-data">
+    <?php foreach ( $data as $label=>$value ) { ?>
+	<h4 title=""><?php echo $label; ?></h4>
+	<p><?php echo $value; ?></p>
+    <?php } ?>
+</div>
+<?php }
