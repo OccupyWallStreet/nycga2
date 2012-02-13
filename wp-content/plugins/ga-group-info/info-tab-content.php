c4950b7ce43c6babdba3ad5b5e6185c6a34ce176
Louie McCoy <louie@louiemccoy.com>
2012-02-12 19:21:40 -0500

    removed function identifier; added test $data

--- wp-content/plugins/ga-group-info/info-tab-content.php
+++ wp-content/plugins/ga-group-info/info-tab-content.php
@@ -3,9 +3,15 @@
 // put them into a multi-dimensional array
 gait_display_info_content();
 
-function gait_display_info_content(){ ?>
+function gait_display_info_content(){
+    global $bp;
+    $data = array(
+	'Name' => 'Indigo Montoya',
+	'Who did you kill?' => 'My Father',
+	'What should we prepare for?' => 'to die.'
+    );
+    ?>
 <div class="extra-data">
-    gait_display_info_content();
     <?php foreach ( $data as $label=>$value ) { ?>
 	<h4 title=""><?php echo $label; ?></h4>
 	<p><?php echo $value; ?></p>
