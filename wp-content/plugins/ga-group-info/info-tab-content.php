5b3bc3fa8bc09b8e04b28595bce9475c463b449c
Louie McCoy <louie@louiemccoy.com>
2012-03-03 13:40:10 -0500

    Removed temporary code to see the insides of contact_fields data

--- wp-content/plugins/ga-group-info/info-tab-content.php
+++ wp-content/plugins/ga-group-info/info-tab-content.php
@@ -1,10 +1,6 @@
 <?php
     global $bp;
     $data_store = groups_get_groupmeta( $bp->groups->current_group->id, $this->slug );
-    
-    // temp for figuring out how old contact info is stored
-    $fields = json_decode( groups_get_groupmeta( $bp->groups->current_group->id, 'contact_fields' ) );
-    print_r($fields);
     ?>
 <div class="extra-data">
     <?php foreach ( $data_store as $slug => $data ) {
