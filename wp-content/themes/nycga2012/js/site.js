305da7c84b38cff1ec6c975a6884d89d55f962f2
Thiago de Mello Bueno <thiago@madeofpeople.org>
2012-03-17 19:28:46 -0400

    removed adddropshadow from site.js
    Ajax loaded content wasnt being 'blessed' by the dropshadow
    Given the difficulty of hooking into ajax buddypress bits, it's easier
    to do via css.

--- wp-content/themes/nycga2012/js/site.js
+++ wp-content/themes/nycga2012/js/site.js
@@ -2,11 +2,11 @@
 
 jQuery(document).ready(function() {
 
-	// add drop shadow to any element with the '.shadow' class
-	var shadows = jQuery(document).find('.shadow');
-	shadows.each( function( i, element ){
-		nycga.ui.addDropShadow( element );
-	});
+	// // add drop shadow to any element with the '.shadow' class
+	// var shadows = jQuery(document).find('.shadow');
+	// shadows.each( function( i, element ){
+	// 	nycga.ui.addDropShadow( element );
+	// });
 
 	// show a tooltip for each element with the '.hoverMenu' class
 	// code.drewwilson.com/entry/tiptip-jquery-plugin
