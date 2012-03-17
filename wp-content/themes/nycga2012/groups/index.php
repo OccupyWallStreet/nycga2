9e31a4639d0219b0c1cd5712f1e64c6ac2974a58
Thiago de Mello Bueno <thiago@madeofpeople.org>
2012-03-17 19:26:52 -0400

    Removed events from group listing page.
    Events show up almost everywhere, this page is about sorting through
    groups.
    Let's give it room.

--- wp-content/themes/nycga2012/groups/index.php
+++ wp-content/themes/nycga2012/groups/index.php
@@ -13,32 +13,23 @@
 
 	<?php do_action( 'bp_before_directory_groups_page' ); ?>
 
-	<?php locate_template( array( 'leftsidebar.php' ), true ) ?>
+	<?php /* locate_template( array( 'leftsidebar.php' ), true ) */ ?>
 
 	<div id="content" class="grid_19 clearFix">
-		
-		<div class="">
-
-		<?php do_action( 'bp_before_directory_groups' ); ?>
-
 		<form action="" method="post" id="groups-directory-form" class="dir-form">
-
+			<?php do_action( 'bp_before_directory_groups' ); ?>
 			<h3><?php _e( 'Groups Directory', 'buddypress' ); ?><?php if ( is_user_logged_in() && bp_user_can_create_groups() ) : ?> &nbsp;<a class="button" href="<?php echo trailingslashit( bp_get_root_domain() . '/' . bp_get_groups_root_slug() . '/create' ); ?>"><?php _e( 'Create a Group', 'buddypress' ); ?></a><?php endif; ?></h3>
-
 			<?php do_action( 'bp_before_directory_groups_content' ); ?>
-
 			<?php do_action( 'template_notices' ); ?>
-
 			<div class="item-list-tabs" role="navigation">
 				<ul>
 					<li class="selected" id="groups-all"><a href="<?php echo trailingslashit( bp_get_root_domain() . '/' . bp_get_groups_root_slug() ); ?>"><?php printf( __( 'All Groups <span class="itemCount">%s</span>', 'buddypress' ), bp_get_total_group_count() ); ?></a></li>
 					<?php if ( is_user_logged_in() && bp_get_total_group_count_for_user( bp_loggedin_user_id() ) ) : ?>
-						<li id="groups-personal"><a href="<?php echo trailingslashit( bp_loggedin_user_domain() . bp_get_groups_slug() . '/my-groups' ); ?>"><?php printf( __( 'My Groups <span class="itemCount">%s</span>', 'buddypress' ), bp_get_total_group_count_for_user( bp_loggedin_user_id() ) ); ?></a></li>
+					<li id="groups-personal"><a href="<?php echo trailingslashit( bp_loggedin_user_domain() . bp_get_groups_slug() . '/my-groups' ); ?>"><?php printf( __( 'My Groups <span class="itemCount">%s</span>', 'buddypress' ), bp_get_total_group_count_for_user( bp_loggedin_user_id() ) ); ?></a></li>
 					<?php endif; ?>
 					<?php do_action( 'bp_groups_directory_group_filter' ); ?>
 				</ul>
 			</div><!-- .item-list-tabs -->
-
 			<div class="item-list-tabs" id="subnav" role="navigation">
 				<ul class="floatLeft">
 					<?php do_action( 'bp_groups_directory_group_types' ); ?>
@@ -57,28 +48,19 @@
 					<?php bp_directory_groups_search_form() ?>
 				</div><!-- #group-dir-search -->
 			</div>
-
-			<div id="groups-dir-list" class="groups dir-list">
-
-				<?php locate_template( array( 'groups/groups-loop.php' ), true ); ?>
-
-			</div><!-- #groups-dir-list -->
-
-			<?php do_action( 'bp_directory_groups_content' ); ?>
-
-			<?php wp_nonce_field( 'directory_groups', '_wpnonce-groups-filter' ); ?>
-
-			<?php do_action( 'bp_after_directory_groups_content' ); ?>
-
+			<div id='groupListing' class="clearFix">
+				<div id="groups-dir-list" class="groups dir-list">
+					<?php locate_template( array( 'groups/groups-loop.php' ), true ); ?>
+				</div><!-- #groups-dir-list -->
+				<?php do_action( 'bp_directory_groups_content' ); ?>
+				<?php wp_nonce_field( 'directory_groups', '_wpnonce-groups-filter' ); ?>
+				<?php do_action( 'bp_after_directory_groups' ); ?>
+				<?php do_action( 'bp_after_directory_groups_content' ); ?>
+			</div>
 		</form><!-- #groups-directory-form -->
-
-		<?php do_action( 'bp_after_directory_groups' ); ?>
-
-		</div><!-- .padder -->
+		<?php do_action( 'bp_after_directory_groups_page' ); ?>
 	</div><!-- #content -->
 
-	<?php do_action( 'bp_after_directory_groups_page' ); ?>
-
-<?php get_sidebar( 'buddypress' ); ?>
-<?php get_footer( 'buddypress' ); ?>
+		<?php get_sidebar( 'buddypress' ); ?>
+		<?php get_footer( 'buddypress' ); ?>
 
