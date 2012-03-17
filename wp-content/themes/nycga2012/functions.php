e4f86342525b6f34567431e6f48413db4cd094fc
Thiago de Mello Bueno <thiago@madeofpeople.org>
2012-03-17 15:56:21 -0400

    jquery.scrollTo removed from repo, it was vesigeous.

--- wp-content/themes/nycga2012/functions.php
+++ wp-content/themes/nycga2012/functions.php
@@ -85,13 +85,11 @@ add_filter('bp_search_form_type_select','my_bp_search_form_type_select');
 function add_script() {
 	if (!is_admin()) {
 		wp_register_script('jquery', 'http://ajax.googleapis.com/ajax/libs/jquery/1.5.2/jquery.min.js', false, '1.5.2');
-		// wp_register_script('jqueryscrollto', get_stylesheet_directory_uri()  . '/js/jquery.scrollTo-min.js', false, '0.5');
 		wp_register_script('boxShadow', get_stylesheet_directory_uri()  . '/js/boxShadow.js', false, '0.5');
 		wp_register_script('tipTip', get_stylesheet_directory_uri()  . '/js/jquery.tipTip.js', false, '1.3	');
 		wp_register_script('nycgaui', get_stylesheet_directory_uri()  . '/js/nycga_ui.js', false, '0.5');
 		wp_register_script('site', get_stylesheet_directory_uri()  . '/js/site.js', false, '0.5');
 		wp_enqueue_script('jquery');
-		wp_enqueue_script('jqueryscrollto');
 		wp_enqueue_script('toggler', get_bloginfo('url') . '/wp-content/js/hide-form/toggler.js');
 		wp_enqueue_script('boxShadow');
 		wp_enqueue_script('tipTip');
