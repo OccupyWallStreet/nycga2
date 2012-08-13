3fdc102c9cd43208b1c451429759afbdb797b72e
Pea <patricia@nycga.net>
2012-08-13 17:02:35 -0400

    Interim Update
    Added List Category Posts plugin.

---
+++ wp-content/plugins/list-category-posts/include/lcp_widget_form.php
@@ -0,0 +1,143 @@
+<?php
+/**
+ * List Category Posts sidebar widget form for Appearance > Widgets.
+ * @author fernando@picandocodigo.net
+ */
+$default = array (
+                'title' => '',
+                'categoryid' => '',
+                'limit' => '',
+                'orderby'=>'',
+                'order'=>'',
+                'show_date'=>'',
+                'show_author'=>'',
+                'show_excerpt'=>'',
+                'exclude'=>'',
+                'excludeposts'=>'',
+                'thumbnail' =>'',
+                'offset'=>'',
+                'show_catlink'=>'',
+                'morelink' =>'' 
+                );
+$instance = wp_parse_args( (array) $instance, $default);
+$title = strip_tags($instance['title']);
+$limit = strip_tags($instance['limit']);
+$orderby = strip_tags($instance['orderby']);
+$order = strip_tags($instance['order']);
+$showdate = strip_tags($instance['show_date']);
+$showauthor = strip_tags($instance['show_author']);
+$exclude = strip_tags($instance['exclude']);
+$excludeposts = strip_tags($instance['excludeposts']);
+$offset = strip_tags($instance['offset']);
+$showcatlink = strip_tags($instance['show_catlink']);
+$categoryid = strip_tags($instance['categoryid']);
+$showexcerpt = strip_tags($instance['show_excerpt']);
+$thumbnail = strip_tags($instance['thumbnail']);
+$thumbnail_size = strip_tags($instance['thumbnail_size']);
+$morelink = strip_tags($instance['morelink']);
+?>
+
+<p><label for="<?php echo $this->get_field_id('title'); ?>"><?php _e("Title", 'list-category-posts')?></label>
+<br/>
+        <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" 
+        name="<?php echo $this->get_field_name('title'); ?>" type="text" 
+        value="<?php echo esc_attr($title); ?>" />
+</p>
+
+<p><label for="<?php echo $this->get_field_id('categoryid'); ?>"><?php _e("Category", 'list-category-posts')?></label>
+<br/>
+<select id="<?php echo $this->get_field_id('categoryid'); ?>" name="<?php echo $this->get_field_name('categoryid'); ?>">
+        <?php 
+                $categories=  get_categories();
+                foreach ($categories as $cat) :
+                        $option = '<option value="' . $cat->cat_ID . '" ';
+                        if ($cat->cat_ID == $categoryid) :
+                                $option .= ' selected = "selected" ';
+                        endif;
+                        $option .=  '">';
+                        $option .= $cat->cat_name;
+                        $option .= '</option>';
+                        echo $option;
+                endforeach;
+        ?>
+</select></p>
+
+<p><label for="<?php echo $this->get_field_id('limit'); ?>"><?php _e("Number of posts", 'list-category-posts')?></label>
+<br/>
+        <input size="2" id="<?php echo $this->get_field_id('limit'); ?>" 
+        name="<?php echo $this->get_field_name('limit'); ?>" type="text" 
+        value="<?php echo esc_attr($limit); ?>" />
+</p>
+
+<p><label for="<?php echo $this->get_field_id('offset'); ?>"><?php _e("Offset", 'list-category-posts')?>: <br/>
+        <input size="2" id="<?php echo $this->get_field_id('offset'); ?>" 
+        name="<?php echo $this->get_field_name('offset'); ?>" type="text" 
+        value="<?php echo esc_attr($offset); ?>" />
+</label></p>
+
+<p><label for="<?php echo $this->get_field_id('orderby'); ?>"><?php _e("Order by", 'list-category-posts')?></label> <br/>
+        <select  id="<?php echo $this->get_field_id('orderby'); ?>" 
+                name="<?php echo $this->get_field_name('orderby'); ?>" type="text" >
+                <option value='date'><?php _e("Date", 'list-category-posts')?></option>
+                <option value='title'><?php _e("Post title", 'list-category-posts')?></option>
+                <option value='author'><?php _e("Author", 'list-category-posts')?></option>
+                <option value='rand'><?php _e("Random", 'list-category-posts')?></option>
+        </select>
+</p>
+
+<p><label for="<?php echo $this->get_field_id('order'); ?>"><?php _e("Order", 'list-category-posts')?></label><br/>
+        <select id="<?php echo $this->get_field_id('order'); ?>" 
+                name="<?php echo $this->get_field_name('order'); ?>" type="text">
+                <option value='desc'><?php _e("Descending", 'list-category-posts')?></option>
+                <option value='asc'><?php _e("Ascending", 'list-category-posts')?></option>
+        </select>
+</p>
+
+<p><label for="<?php echo $this->get_field_id('exclude'); ?>"><?php _e("Exclude categories (id's)", 'list-category-posts')?></label><br/>
+        <input id="<?php echo $this->get_field_id('exclude'); ?>"
+        name="<?php echo $this->get_field_name('exclude'); ?>" type="text"
+        value="<?php echo esc_attr($exclude); ?>" />
+</p>
+
+<p><label for="<?php echo $this->get_field_id('excludeposts'); ?>"><?php _e("Exclude posts (id's)", 'list-category-posts')?></label><br/>
+        <input id="<?php echo $this->get_field_id('excludeposts'); ?>"
+        name="<?php echo $this->get_field_name('excludeposts'); ?>" type="text"
+        value="<?php echo esc_attr($excludeposts); ?>" />
+</p>
+
+
+<label><?php _e("Show", 'list-category-posts')?>: </label><br/>
+<p>
+    <input type="checkbox" <?php checked( (bool) $instance['thumbnail'], true ); ?>
+    name="<?php echo $this->get_field_name( 'thumbnail'); ?>" /> <?php _e("Thumbnail - size", 'list-category-posts')?> 
+    <select id="<?php echo $this->get_field_id('thumbnail_size'); ?>"
+        name="<?php echo $this->get_field_name( 'thumbnail_size' ); ?>" type="text">
+        <option value='thumbnail'>thumbnail</option>
+        <option value='medium'>medium</option>
+        <option value='large'>large</option>
+        <option value='full'>full</option>
+    </select>
+</p>
+<p>
+    <input class="checkbox"  type="checkbox" <?php checked($instance['show_date'], true ); ?>
+    name="<?php echo $this->get_field_name( 'show_date' ); ?>" /> <?php _e("Date", 'list-category-posts')?>
+</p>
+<p>
+    <input class="checkbox" input type="checkbox" <?php checked($instance['show_author'], true ); ?>
+    name="<?php echo $this->get_field_name( 'show_author' ); ?>" /> <?php _e("Author", 'list-category-posts')?>
+</p>
+<p>
+    <input class="checkbox" input type="checkbox" <?php checked($instance['show_catlink'], true ); ?>
+    name="<?php echo $this->get_field_name( 'show_catlink' ); ?>" /> <?php _e("Link to category", 'list-category-posts')?>
+</p>
+<p>
+    <input class="checkbox" input type="checkbox" <?php checked($instance['show_excerpt'], true ); ?>
+    name="<?php echo $this->get_field_name( 'show_excerpt' ); ?>" /> <?php _e("Excerpt", 'list-category-posts')?>
+</p>
+<p>
+    <label for="<?php echo $this->get_field_id('morelink'); ?>"><?php _e("More link", 'list-category-posts')?>: <br/>
+    <input class="widefat" id="<?php echo $this->get_field_id('morelink'); ?>" 
+        name="<?php echo $this->get_field_name('morelink'); ?>" type="text" 
+        value="<?php echo esc_attr($morelink); ?>" />
+    </p>
+
---
+++ wp-content/plugins/list-category-posts/templates/default.php
@@ -0,0 +1,93 @@
+<?php
+/*
+Plugin Name: List Category Posts - Template
+Plugin URI: http://picandocodigo.net/programacion/wordpress/list-category-posts-wordpress-plugin-english/
+Description: Template file for List Category Post Plugin for Wordpress which is used by plugin by argument template=value.php
+Version: 0.9
+Author: Radek Uldrych & Fernando Briano 
+Author URI: http://picandocodigo.net http://radoviny.net
+*/
+
+/* Copyright 2009  Radek Uldrych  (email : verex@centrum.cz), Fernando Briano (http://picandocodigo.net)
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3 of the License, or 
+any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+/**
+ * The format for templates changed since version 0.17.
+ * Since this code is included inside CatListDisplayer, $this refers to
+ * the instance of CatListDisplayer that called this file.
+ */
+
+/* This is the string which will gather all the information.*/
+$lcp_display_output = '';
+
+// Show category link:
+$lcp_display_output .= $this->get_category_link('strong');
+
+//Add 'starting' tag. Here, I'm using an unordered list (ul) as an example:
+$lcp_display_output .= '<ul class="lcp_catlist">';
+
+/**
+ * Posts loop.
+ * The code here will be executed for every post in the category.
+ * As you can see, the different options are being called from functions on the
+ * $this variable which is a CatListDisplayer.
+ *
+ * The CatListDisplayer has a function for each field we want to show.
+ * So you'll see get_excerpt, get_thumbnail, etc.
+ * You can now pass an html tag as a parameter. This tag will sorround the info
+ * you want to display. You can also assign a specific CSS class to each field.
+ */
+foreach ($this->catlist->get_categories_posts() as $single):
+    //Start a List Item for each post:
+    $lcp_display_output .= "<li>";
+
+    //Show the title and link to the post:
+    $lcp_display_output .= $this->get_post_title($single);
+
+    //Show comments:
+    $lcp_display_output .= $this->get_comments($single);
+
+    //Show date:
+    $lcp_display_output .= ' ' . $this->get_date($single);
+
+    //Show author
+    $lcp_display_output .= $this->get_author($single);
+
+    //Custom fields:
+    $lcp_display_output .= $this->get_custom_fields($this->params['customfield_display'], $single->ID);
+
+    //Post Thumbnail
+    $lcp_display_output .= $this->get_thumbnail($single);
+
+    /**
+     * Post content - Example of how to use tag and class parameters:
+     * This will produce:<p class="lcp_content">The content</p>
+     */
+    $lcp_display_output .= $this->get_content($single, 'p', 'lcp_content');
+
+    /**
+     * Post content - Example of how to use tag and class parameters:
+     * This will produce:<div class="lcp_excerpt">The content</div>
+     */
+    $lcp_display_output .= $this->get_excerpt($single, 'div', 'lcp_excerpt');
+
+    //Close li tag
+    $lcp_display_output .= '</li>';
+endforeach;
+
+$lcp_display_output .= '</ul>';
+$this->lcp_output = $lcp_display_output;
